64371ad492e1285f8ecc86e66cfb1a8e68699846
Marwan Baz <kaka.mer1998@gmail.com>
2022-12-28 23:22:27 +0300

    complete assignmen

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(dic) {
   // Write your code here!
+  if (dic <= 400) {
+    return "This one is on me!";
+  } else if (dic >= 400 < 1999) {
+    return "That will be twenty bucks.";
+  }else if(dic > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }else if (dic > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  // const city = "NYC"
+  return city == "NYC" ? "Ok, sounds good."  : "No go." ;
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  const gen = "generous";
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case !"generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
 }
